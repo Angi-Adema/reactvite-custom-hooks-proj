8736f100b252d405e1ccf967249b9f1fd51c6e3d
CoderGirl <angi.adema@gmail.com>
2023-10-16 14:29:29 -0600

    added fetch code to useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,1 +1,23 @@
+import { useEffect, useState } from "react";
 
+export function useFetch(url) {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+}
